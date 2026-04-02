cff55a934a32eb3d357d94720e4ae7ed308c9b3a
Fnav05 <felvinnavarro10@gmail.com>
2026-04-02 14:58:44 -0700

    A59 pass

--- main.cpp
+++ main.cpp
@@ -5,7 +5,19 @@ using namespace std;
 int main()
 {
 	int prev, current;
+	
 	// TODO
+	int flag = 1;
+	
+	cin >> current;
 
 	// TODO
+	while (flag == 1 || prev > current) {
+		cout << current << "\t";
+		flag = 0;
+		prev = current;
+		cin >> current;
+	}
+	cout << endl;
+	return 0;
 }
